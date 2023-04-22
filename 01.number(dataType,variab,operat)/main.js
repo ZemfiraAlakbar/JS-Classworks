09e37939b3ef735ec2a29a3d4caaf0ae221ba489
ZemfiraAlakbar <127456372+ZemfiraAlakbar@users.noreply.github.com>
2023-04-22 13:39:46 -0700

    Add files via upload

---
+++ 01.number(dataType,variab,operat)/main.js
@@ -0,0 +1,34 @@
+console.log(4+3+"5"+2+"a")
+console.log(12*8)
+let a=10
+let b=5
+let x=a/b
+console.log(x)
+let num1=20
+let num2=17
+console.log(num1+num2)
+let fullname="zenfira", surname="alakbarova", year="2001"
+let info=fullname+surname+year
+console.log(info)
+let f=12
+let u=5
+console.log (f%u)
+let cityName="baku" 
+let cityName1="Gence"
+console.log(cityName1)
+let obj1 = {
+    name:"someone"
+}
+console.log(obj1);
+let obj2 = {
+    age:"20"
+}
+console.log(obj2)
+let cityNAME = {yourciy: "baku", uni: "gence"};
+let newObj = {...cityName3 , yourcity:"gence"}
+console.log
+let text = "there";
+console.log(`hello ${3+8}`);
+console.log(`hello ${`3+8`}`);
+console.log(`hello ${"text"}`);
+console.log(`hello ${text}`);

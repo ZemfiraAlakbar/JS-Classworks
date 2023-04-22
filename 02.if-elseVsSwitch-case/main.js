2f413175927f61acecbae24905991097c89d8351
ZemfiraAlakbar <127456372+ZemfiraAlakbar@users.noreply.github.com>
2023-04-22 14:32:28 -0700

    Add files via upload

---
+++ 02.if-elseVsSwitch-case/main.js
@@ -0,0 +1,19 @@
+for(i=0;i<10;i++){
+    if (i%2==1) {
+    console.log(i); 
+    }
+}
+
+let sum = 0
+for(i=0;i<10;i++){
+    if (i%2==1) {
+    console.log(sum=i+1); 
+    } 
+}
+
+let age = 21
+if (age >= 18 && age <= 29) {
+    console.log('you are allowed');
+} else {
+    console.log('you are not allowed');
+}

f0ba571ac9af657f061913b199dd59086eb9c879
ZemfiraAlakbar <127456372+ZemfiraAlakbar@users.noreply.github.com>
2023-04-22 14:12:30 -0700

    Add files via upload

---
+++ 03.loops(for,while)/main.js
@@ -0,0 +1,22 @@
+// 1
+console.log('task 1');
+let myName= 'Zenfira'
+for (let i=0;i<myName.length;i++){
+console.log(myName[i]);
+}
+
+// 2
+console.log('task 2');
+for(let i=1;i<8;i++){
+    if(i%2==0) {
+        console.log(i);
+    }
+}
+
+// 3
+console.log('task 3');
+for (i=1;i<=7;i++){
+    if (i%2==0) {
+        console.log(`${i} is even`);
+    } else {console.log(`${i} is odd`);}
+}
